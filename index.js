81b09ab4869ee3c9d4cc3778dc7632db8bf97f7d
Koen <koen.bonenkamp@gmail.com>
2023-12-01 16:05:48 +0100

    add test code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const fs = require("fs");
-const path = require("path");
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -10,14 +9,13 @@ try {
   const time = new Date().toTimeString();
   core.setOutput("time", time);
 
-  const requirementsPath = path.join(github.workspace, "requirements.txt");
-  const requirements = fs.readFileSync(requirementsPath, "utf8");
+  const requirements = fs.readFileSync("requirements.txt", "utf8");
   const firstLine = requirements.split("\n")[0];
   console.log(`First line of requirements.txt: ${firstLine}`);
 
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
